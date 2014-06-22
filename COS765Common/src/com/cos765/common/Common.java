0b346fa53fd5cef360edd07cd6f82efc8b67b522
gustavogbc@gmail.com <gustavogbc@gmail.com@5cc2164b-378e-463d-a535-f1f3b5c02552>
2014-06-22 03:54:11 +0000

    novo cliente. flag de buffer cheio movida para Common

--- COS765Common/src/com/cos765/common/Common.java
+++ COS765Common/src/com/cos765/common/Common.java
@@ -3,5 +3,6 @@ package com.cos765.common;
 public class Common {
 
 	public static final int SERVER_PORT = 15000;
-	public static final int TRANSMISSION_TIME = 20;	
+	public static final int TRANSMISSION_TIME = 20;
+	public static Boolean bufferFull = false;	
 }
