3ce4d3dae501c9051c5276b44b36a9743f6c224e
gustavogbc@gmail.com <gustavogbc@gmail.com@5cc2164b-378e-463d-a535-f1f3b5c02552>
2014-06-26 18:24:19 +0000

    falta apenas o numero de pacotes nao tocados

--- COS765Common/src/com/cos765/common/Common.java
+++ COS765Common/src/com/cos765/common/Common.java
@@ -6,17 +6,19 @@ public class Common {
 	public static final int TRANSMISSION_TIME = 20;
 	public static Boolean bufferFull = false;	
 	public static int maxBufferSize = 10;
+	public static boolean returnedFromPause = false;
 	
 	
 	public static class Statistics {
 		// Statistics
+		public static int receivedSegments = 0;
 		public static int expiredSegments = 0;
 		public static int lostSegments = 0;
 		public static long initialTransferTime = 0;
 		public static long totalTransferTime = 0;
 		public static int totalTransferSize = 0;		
 		public static double throughput = 0.0;
-		public static int pauseCount = 0;
+		public static int pauseCount = 1;
 		public static long pauseStartTime = 0;
 		public static long pauseEndTime = 0;
 		public static long totalPauseTime = 0;
