f278a369c8aba8799fb36284e18c258d90f31dd7
gustavogbc@gmail.com <gustavogbc@gmail.com@5cc2164b-378e-463d-a535-f1f3b5c02552>
2014-06-26 05:57:46 +0000

    variaveis globais para estatisticas do projeto

--- COS765Common/src/com/cos765/common/Common.java
+++ COS765Common/src/com/cos765/common/Common.java
@@ -4,7 +4,21 @@ public class Common {
 
 	public static final int SERVER_PORT = 15000;
 	public static final int TRANSMISSION_TIME = 20;
-//	public static final int MAX_BUFFER_SIZE = 4;
 	public static Boolean bufferFull = false;	
 	public static int maxBufferSize = 10;
+	
+	
+	public static class Statistics {
+		// Statistics
+		public static int expiredSegments = 0;
+		public static int lostSegments = 0;
+		public static long initialTransferTime = 0;
+		public static long totalTransferTime = 0;
+		public static int totalTransferSize = 0;		
+		public static double throughput = 0.0;
+		public static int pauseCount = 0;
+		public static long pauseStartTime = 0;
+		public static long pauseEndTime = 0;
+		public static long totalPauseTime = 0;
+	}	
 }
